2248f8ae84a5d89a7517bae3c5f9ead4ca2a2502
Lee Chuk Munn <chukmunnlee@gmail.com>
2021-12-08 08:39:15 +0000

    day02 PM demos

---
+++ workshop02/resources.tf
@@ -0,0 +1,26 @@
+data digitalocean_ssh_key mykey {
+    name = "mykey"
+}
+
+resource digitalocean_droplet myserver {
+    name = "myserver"
+    image = var.DO_image
+    size = var.DO_size
+    region = var.DO_region
+
+    ssh_keys = [ data.digitalocean_ssh_key.mykey.fingerprint ]
+}
+
+resource local_file inventory-yaml {
+    filename = "setup/inventory.yaml"
+    file_permission = 0644
+    content = templatefile("inventory.yaml.tpl", {
+        host_name = digitalocean_droplet.myserver.name
+        host_ip = digitalocean_droplet.myserver.ipv4_address
+        private_key = "../${var.private_key}"
+    })
+}
+
+output myserver-ipv4 {
+    value = digitalocean_droplet.myserver.ipv4_address
+}
---
+++ workshop02/variables.tf
@@ -0,0 +1,39 @@
+variable DO_token {
+    type = string
+    sensitive = true
+}
+
+variable DO_image {
+    type = string
+    default = "ubuntu-20-04-x64"
+}
+
+variable DO_size {
+    type = string
+    default = "s-1vcpu-1gb"
+}
+
+variable DO_region {
+    type = string
+    default = "sgp1"
+}
+
+variable CF_email {
+    type = string
+}
+
+variable CF_api_token {
+    type = string
+    sensitive = true
+}
+
+variable CF_zone {
+    type = string
+}
+
+variable private_key {
+    type = string
+}
+variable public_key {
+    type = string
+}
